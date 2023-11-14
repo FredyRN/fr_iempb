4715eb5a3a5cb400fa664986a15959c6eeac430e
FredyRN <jhon@fredyrn.com>
2023-11-14 08:01:21 -0500

    Added from master branch

---
+++ build/app/generated/source/buildConfig/debug/github/comFredyRN/fr_iempb/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package github.comFredyRN.fr_iempb;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "github.comFredyRN.fr_iempb";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
